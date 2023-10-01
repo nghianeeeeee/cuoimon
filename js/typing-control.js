3426db8e28ff144429c6ccecfa6680be22f2fb69
nghianeeeeee <107293616+nghianeeeeee@users.noreply.github.com>
2023-10-01 23:40:45 +0700

    Add files via upload

---
+++ js/typing-control.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+         strings: ["PhotoGrapher.", "Influencer.","Travel Vlogger.","Food Vlogger."],
+    typeSpeed:20,
+        backSpeed:20,
+        loop:true,
+      });
